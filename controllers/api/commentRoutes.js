8e54e0d52eff1e9ce7b0fc32b796ebb16c0c237b
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:50:41 -0400

    Added a DELETE route to delete comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -37,12 +37,38 @@ router.post('/', async (req, res) => {
 
 // PUT update a comment by ID - /api/comments/:id
 router.put('/:id', async (req, res) => {
-  
+    try {
+        const commentData = await Comment.update(req.body, {
+          where: {
+            id: req.params.id,
+          },
+        });
+        if (!commentData) {
+          res.status(404).json({ message: 'No comment found with that id!' });
+          return;
+        }
+        res.status(200).json(commentData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 // DELETE a comment by ID - /api/comments/:id
 router.delete('/:id', async (req, res) => {
-  
+    try {
+        const commentData = await Comment.destroy({
+          where: {
+            id: req.params.id,
+          },
+        });
+        if (!commentData) {
+          res.status(404).json({ message: 'No comment found with that id!' });
+          return;
+        }
+        res.status(200).json(commentData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;
