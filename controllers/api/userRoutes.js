539ea127cea2c697065ace3c5284f192f569a044
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:35:38 -0400

    Added both GET routes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET all users - /api/users/
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
+// GET a single user by ID - /api/users/:id
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        if (!userData) {
+          res.status(404).json({ message: 'No user found with that id!' });
+          return;
+        }
+        res.status(200).json(userData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
+// POST a new user - /api/users/
+router.post('/', async (req, res) => {
+
+});
+
+// PUT update a user by ID - /api/users/:id
+router.put('/:id', async (req, res) => {
+
+});
+
+// DELETE a user by ID - /api/users/:id
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
