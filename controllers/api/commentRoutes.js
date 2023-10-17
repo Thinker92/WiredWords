e4bcec2ad7ee8d3608d096a84a095d9caf484eb4
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:46:19 -0400

    Setup commentRoutes.js

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// GET all comments - /api/comments/
+router.get('/', async (req, res) => {
+  
+});
+
+// GET a single comment by ID - /api/comments/:id
+router.get('/:id', async (req, res) => {
+  
+});
+
+// POST a new comment - /api/comments/
+router.post('/', async (req, res) => {
+  
+});
+
+// PUT update a comment by ID - /api/comments/:id
+router.put('/:id', async (req, res) => {
+  
+});
+
+// DELETE a comment by ID - /api/comments/:id
+router.delete('/:id', async (req, res) => {
+  
+});
+
+module.exports = router;
