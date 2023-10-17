3e7861bd39cd51667052845c96417460f6450c63
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:38:00 -0400

    Added POST and PUT routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -27,12 +27,30 @@ router.get('/:id', async (req, res) => {
 
 // POST a new user - /api/users/
 router.post('/', async (req, res) => {
-
+    try {
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 // PUT update a user by ID - /api/users/:id
 router.put('/:id', async (req, res) => {
-
+    try {
+        const userData = await User.update(req.body, {
+          where: {
+            id: req.params.id,
+          },
+        });
+        if (!userData) {
+          res.status(404).json({ message: 'No user found with that id!' });
+          return;
+        }
+        res.status(200).json(userData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 // DELETE a user by ID - /api/users/:id
