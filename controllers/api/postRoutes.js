8c5e2ffadb5527210c605c5e52d5735e0cdf0bdd
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:42:10 -0400

    Setup post GET all Route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,7 +3,12 @@ const { Post } = require('../../models');
 
 // GET all posts - /api/posts/
 router.get('/', async (req, res) => {
-    
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 // GET a single post by ID - /api/posts/:id
