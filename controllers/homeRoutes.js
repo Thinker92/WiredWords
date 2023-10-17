79f4939263e4ef97a9f04a885d92798c93729d08
Thinker92 <vilsonapo@gmail.com>
2023-10-16 22:00:15 -0400

    Added Login Route and Homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,5 +2,23 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
 // Setup Homepage route
-// Setup Login page route
+router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        include: [{ model: User }],
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('homepage', { posts });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+// GET login page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 // Setup Dashboard route
