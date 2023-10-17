8f7c2360245e9243afdc5c9470ca11f43d72eff4
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:50:00 -0400

    Added PUT and POST routes for updating and posting new comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -27,7 +27,12 @@ router.get('/:id', async (req, res) => {
 
 // POST a new comment - /api/comments/
 router.post('/', async (req, res) => {
-  
+    try {
+        const commentData = await Comment.create(req.body);
+        res.status(200).json(commentData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 // PUT update a comment by ID - /api/comments/:id
