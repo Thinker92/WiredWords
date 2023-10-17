5443bea9efaddc62d9e59f721cc775e75cd5b871
Thinker92 <vilsonapo@gmail.com>
2023-10-16 21:48:53 -0400

    Added GET routes for all and one comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,12 +3,26 @@ const { Comment } = require('../../models');
 
 // GET all comments - /api/comments/
 router.get('/', async (req, res) => {
-  
+    try {
+        const commentData = await Comment.findAll();
+        res.status(200).json(commentData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 // GET a single comment by ID - /api/comments/:id
 router.get('/:id', async (req, res) => {
-  
+    try {
+        const commentData = await Comment.findByPk(req.params.id);
+        if (!commentData) {
+          res.status(404).json({ message: 'No comment found with that id!' });
+          return;
+        }
+        res.status(200).json(commentData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 // POST a new comment - /api/comments/
